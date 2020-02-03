98c941c715b40a0329e1d71459b4af5ec83a4858
jdehan2 <jdehan2@F1092-47-2320>
2020-02-03 15:16:08 -0600

    Sides update

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the 
+    /// </summary>
+    public class ChiliCheeseFries : Side
+    {
+
+        public override uint calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 433;
+                    case Size.Medium:
+                        return 534;
+                    case Size.Large:
+                        return 610;
+                }
+            }
+        }
+
+    }
+}
