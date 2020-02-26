62116812dc46663e43b00a6622112be855f937f8
jdehan2 <jdehan2@F1092-47-2320>
2020-02-26 14:48:38 -0600

    new tests

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        double Price { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+
+
+    }
+}
